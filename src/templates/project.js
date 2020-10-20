33930e68c4c8a1175e10f10d878319996a619d09
Luuk <luuk@afdelingbuitengewonezaken.nl>
2020-10-20 23:06:13 +0200

    last minute stuff

--- src/templates/project.js
+++ src/templates/project.js
@@ -4,56 +4,25 @@ import React, { useEffect, useState } from "react";
 import "../style/project.css";
 import Draggable from "react-draggable";
 import limaLogo from "../assets/LIMA_logo_staand_wit.png";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 
-function handleLoad() {
-  var divs = document.getElementsByClassName("window");
-  var winWidth = window.innerWidth;
-  var winHeight = window.innerHeight;
-
-  for (var i = 0; i < divs.length; i++) {
-    var thisDiv = divs[i];
-    const randomTop = getRandomNumber(0, winHeight);
-    const randomLeft = getRandomNumber(0, winWidth);
-    thisDiv.style.top = randomTop + "px";
-    thisDiv.style.left = randomLeft + "px";
-  }
-
-  function getRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-}
-
-/*
-{
-  "data": {
-    "allprojects": {
-      "edges": [
-        {
-          "node": {
-            "slug": "rafaelrozendaal",
-            "title": "Rafaël Rozendaal"
-          }
-        },
-        {
-          "node": {
-            "slug": "luuk",
-            "title": "Amalia Ulman"
-          }
-        }
-      ]
-    }
-  },
-  "extensions": {}
-}
-*/
 export default ({ data }) => {
-  useEffect(handleLoad, []);
   const project = data.datoCmsProject;
   const [openWindows, setOpenWindows] = useState(
     project.links.map((link) => link.open)
   );
-  const [zIndexes, setZIndexes] = useState(project.links.map(() => 0));
+  const [zIndexes, setZIndexes] = useState(
+    project.links.map((link) => (link.open ? 0 : 1))
+  );
+  const [defaultPositions] = useState(
+    project.links.map(() => ({
+      x: Math.max(0, Math.random() * (window.innerWidth - 200)),
+      y: Math.random() * (window.innerHeight - 18),
+    }))
+  );
   const [selectedWindow, setSelectedWindow] = useState(null);
+  const [dragging, setDragging] = useState(false);
+  const [showAbout, setShowAbout] = useState(false);
   return (
     <div
       className="projectcontainer"
@@ -83,13 +52,40 @@ export default ({ data }) => {
       </div>
 
       <div
-        className="h1box2"
+        className="h1box2 bottom-menu"
         style={{
           background: project.menucolor.hex,
           color: project.textcolor.hex,
         }}
       >
-        <p>Cultural Matter</p>
+        <p
+          className="bottom-menu--title"
+          onClick={() => setShowAbout(!showAbout)}
+        >
+          <span className="bottom-menu--icon">
+            {showAbout && <FaChevronDown />}
+            {!showAbout && <FaChevronUp />}
+          </span>
+          Cultural Matter
+        </p>
+        {showAbout && (
+          <p className="bottom-menu--body">
+            Digital culture keeps on coming up with the newest, the best and the
+            fastest. But what do we know about its history? Artists have been
+            designing, critiquing and manipulating new technologies for decades.
+            Work by earlier Internet artists, post-Internet artists, hackers and
+            media activists is no longer always understood in the light of the
+            current state of technology, while it still has a lot to say about
+            contemporary technology – and art. These ‘media artists’ have left
+            their mark on our current society through their critical use of new
+            technologies. Thanks to their awareness of, and break with, art
+            history, they can justifiably be called the new avant-garde.
+            Cultural Matter highlights the enduring expressive power of digital
+            artworks: works in which art and technology and the past and the
+            future come together in a way that is as logical as it is
+            groundbreaking.
+          </p>
+        )}
       </div>
       <div
         className="h1box3"
@@ -108,40 +104,72 @@ export default ({ data }) => {
       {/* links loopen */}
       {project.links.map((link, i) => {
         return (
-          <Draggable>
+          <Draggable
+            onStart={() => setDragging(true)}
+            onStop={(e) => {
+              setDragging(false);
+            }}
+            defaultPosition={defaultPositions[i]}
+            bounds={{ top: 0 }}
+          >
             <div
               key={`link-${i}`}
               className={`window ${openWindows[i] ? "open" : ""} ${
                 selectedWindow === i ? "selected" : ""
               }`}
-              style={{ background: link.onecolor.hex, zIndex: zIndexes[i] }}
-              onDoubleClick={() =>
-                setOpenWindows(
-                  openWindows.map((windowOpen, windowOpenIndex) =>
-                    windowOpenIndex === i ? !windowOpen : windowOpen
-                  )
-                )
-              }
-              onMouseDownCapture={() => {
-                const highestZIndex = zIndexes.reduce(
-                  (acc, val) => (val > acc ? val : acc),
-                  0
-                );
-                setZIndexes(
-                  zIndexes.map((zIndex, zIndexIndex) =>
-                    zIndexIndex === i ? highestZIndex + 1 : zIndex
-                  )
-                );
-                setSelectedWindow(i);
+              style={{
+                background: link.onecolor ? link.onecolor.hex : undefined,
+                zIndex: zIndexes[i],
               }}
             >
-              <p style={{ color: project.textcolor.hex }}>{link.itemtitle}</p>
+              <p
+                className="window-title"
+                onDoubleClick={() =>
+                  setOpenWindows(
+                    openWindows.map((windowOpen, windowOpenIndex) =>
+                      windowOpenIndex === i ? !windowOpen : windowOpen
+                    )
+                  )
+                }
+                onMouseDownCapture={() => {
+                  const highestZIndex = zIndexes.reduce(
+                    (acc, val) => (val > acc ? val : acc),
+                    0
+                  );
+                  setZIndexes(
+                    zIndexes.map((zIndex, zIndexIndex) =>
+                      zIndexIndex === i ? highestZIndex + 1 : zIndex
+                    )
+                  );
+                  setSelectedWindow(i);
+                }}
+                style={{ color: project.textcolor.hex }}
+              >
+                {link.itemtitle}
+              </p>
 
               <div
                 className="box"
                 style={{ height: link.height, width: link.width }}
               >
-                {link.url && <iframe src={link.url} frameBorder="0"></iframe>}
+                {link.textNode && (
+                  <div
+                    className="box-text"
+                    style={{
+                      background: link.onecolor ? link.onecolor.hex : undefined,
+                    }}
+                    dangerouslySetInnerHTML={{
+                      __html: link.textNode.childMarkdownRemark.html,
+                    }}
+                  />
+                )}
+                {link.url && (
+                  <iframe
+                    style={dragging ? { pointerEvents: "none" } : {}}
+                    src={link.url}
+                    frameBorder="0"
+                  ></iframe>
+                )}
                 {link.image && <Img fluid={link.image.fluid} />}
               </div>
             </div>
@@ -183,6 +211,11 @@ export const query = graphql`
         width
         height
         open
+        textNode {
+          childMarkdownRemark {
+            html
+          }
+        }
         onecolor {
           hex
         }
